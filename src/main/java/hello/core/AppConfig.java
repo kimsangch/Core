a8a1099413ea0a0e5cfb28c54eecc4fa0346b95e
kimsangch <mjsc2882@gmail.com>
2022-01-28 22:43:19 +0900

    #스터디 스프링 설정-> xml

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,14 +1,13 @@
 package hello.core;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
-import hello.core.order.RateDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
