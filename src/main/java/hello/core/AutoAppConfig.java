f04ff910df26dd5deec59c832501f4bff9070587
kimsangch <mjsc2882@gmail.com>
2022-02-15 22:03:18 +0900

    #스터디 컴퍼넌트스캔 의존관계 자동 주입 시작

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,9 +6,11 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        //basePackages = "hello.core.member",
+        //basePackageClasses = AutoAppConfig.class,
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION , classes = Configuration.class)
-) //기존에 만든 AppConfig와 충돌이 나서 추가 - 제외하는 코
+) //기존에 만든 AppConfig와 충돌이 나서 추가 - 제외하는 코드
 public class AutoAppConfig {
 
-    
+
 }
